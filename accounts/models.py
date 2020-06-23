2f57b64d0624db12325f8b7ef4fd04f93a5d6c4a
Taein Kim <kti91@likelion.org>
2020-06-23 15:33:03 +0900

    200623 | setup Project

---
+++ accounts/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+from django.urls import reverse
+from imagekit.models import ProcessedImageField
+from imagekit.processors import ResizeToFill
+
+def photo_path(instance, filename):
+    return f'profiles/{instance.username}/{filename}'
+
+# Create your models here.
+class User(AbstractUser):
+
+    GENDER_CHOICES = {
+        ('male', 'Male'),
+        ('female', 'Female'),
+    }
+
+    name = models.CharField("사용자이름", max_langth=100, blank=True)
+    profile_photo = ProcessedImageField(upload_to=photo_path, processors=[ResizeToFill(150, 150)] format='JPEG', options={'quality': 100}, default='null_image.jpg')
+    followers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='followings')
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, blank=True)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.username
+
+    @property
+    def followers_count(self):
+        return self.followers.count()
+
+    @property
+    def following_count(self):
+        return self.following.count()
+
+    # def get_absolute_url(self):
+    #     return reverse()
