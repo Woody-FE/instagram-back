ca7ed82213a7dd462298c8dcb540c2c5f494cbdb
Taein Kim <kti91@likelion.org>
2020-07-12 09:18:43 +0900

    200712 | modified Private

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -14,17 +14,23 @@ class PrivateUserDetailSerializer(serializers.ModelSerializer):
     followers_count = serializers.ReadOnlyField()
     followers = UserFollowList(many=True, read_only=True)
     followings = UserFollowList(many=True, read_only=True)
+    private_user = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'profile_photo', 'name', 'gender', 'description', 'followings', 'followers', 'followers_count', 'following_count', 'is_private')
+        fields = ('id', 'username', 'profile_photo', 'name', 'gender', 'description', 'followings', 'followers', 'followers_count', 'following_count', 'is_private', 'private_user')
+
+    def get_private_user(self, obj):
+        return True
 
 class UserDetailSerializer(PrivateUserDetailSerializer):
     feed_set = SmallFeedSerializer(many=True, read_only=True)
 
     class Meta(PrivateUserDetailSerializer.Meta):
-        fields = ('id', 'username', 'profile_photo', 'name', 'gender', 'description', 'followings', 'followers', 'followers_count', 'following_count', 'is_private', 'feed_set')
+        fields = ('id', 'username', 'profile_photo', 'name', 'gender', 'description', 'followings', 'followers', 'followers_count', 'following_count', 'is_private', 'feed_set', 'private_user')
 
+    def get_private_user(self, obj):
+        return False
 # class UserProfileUpdateSerializer(UserDetailSerializer):
 #     class Meta(UserDetailSerializer.Meta):
         # model = User
